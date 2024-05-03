dd4e5e5c31bf26ceb8eb464db78085f9144b19cd
dan1yal7 <daniiltemirkhanov@gmail.com>
2024-05-03 23:30:34 +0500

    Starting working on controllers

---
+++ API for managing the list of university courses/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using API_for_managing_the_list_of_university_courses.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_for_managing_the_list_of_university_courses.Controllers
+{
+    public class CourseController : Controller
+    {
+
+        private readonly ICourseService _courseservice; 
+
+        public CourseController(ICourseService courseservice)
+        {
+            _courseservice = courseservice;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
