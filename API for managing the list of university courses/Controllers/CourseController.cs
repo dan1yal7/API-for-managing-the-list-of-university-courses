3bc6a08254f53ca56e53ef6f2242e4698835046f
dan1yal7 <daniiltemirkhanov@gmail.com>
2024-05-09 18:55:18 +0500

    GetAll, GetById, Create methods were created in CourseController

--- API for managing the list of university courses/Controllers/CourseController.cs
+++ API for managing the list of university courses/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
-﻿using API_for_managing_the_list_of_university_courses.Services;
+﻿using API_for_managing_the_list_of_university_courses.Models;
+using API_for_managing_the_list_of_university_courses.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,40 @@ namespace API_for_managing_the_list_of_university_courses.Controllers
         public CourseController(ICourseService courseservice)
         {
             _courseservice = courseservice;
+        }
+
+        // GET: api/<CourseController> 
+        [HttpGet] 
+        public ActionResult GetAll()
+        {
+            var courses = _courseservice.GetAll(); 
+            return Ok(courses);
+        }
+
+        // GET: api/<CourseController> 
+        [HttpGet (template: "{id}")] 
+        public ActionResult GetById(int id)
+        {
+            var coruse = _courseservice.GetById(id);
+            return Ok(coruse);
+        }
+
+
+        [HttpPost] 
+
+        public ActionResult Create(Course course)
+        {
+            if(!ModelState .IsValid)
+            {
+
+                return BadRequest(ModelState);
+               
+            }
+
+            _courseservice.Add(course);
+           _courseservice.SaveChanges(); 
+
+           return CreatedAtAction(nameof(GetById), new { id = course.CourseId }, course);
         }
     }
 }
